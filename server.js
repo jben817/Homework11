c07b307207abc18d66a321c216af403ea8979f5c
blue-eyed iguana <61255945+jben817@users.noreply.github.com>
2020-05-16 01:25:06 -0400

    modified index.html, and server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('./css'));
 
 // routes
 app.get("*", function(req, res) {
@@ -17,8 +18,28 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
+// notes
+app.get("/api/notes", function(req, res) {
+  return res.json(data);
+});
+
+// new notes- takes in JSON input
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  console.log(newNote);
+  characters.push(newNote);
+  res.json(newNote);
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+  const index = notes.indexOf(note);
+  notes.splice(index, 1);
+});
+
+
+
 // listening
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("You are listening on PORT " + PORT);
   });
   
