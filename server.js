1cb5beb24c86a6b36f7f8a0bee931794a5558a16
blue-eyed iguana <61255945+jben817@users.noreply.github.com>
2020-05-16 00:32:58 -0400

    updated server and index

--- server.js
+++ server.js
@@ -3,5 +3,22 @@ const fs = require('fs');
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
-const app = express();
 
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
