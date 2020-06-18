06481abee9f742b5253e11cff052f037affca640
j.bennett <61255945+jben817@users.noreply.github.com>
2020-06-18 03:36:05 -0400

    api route updates, created db.json

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,39 @@
-var noteArray = [
-    {
-      title: "",
-      note: "",
-          }
-  ];
+// const express = require("express");
+// const router = express.Router();
+const router = require("express").Router();
+const fs = require("fs");
 
-  // notes
-app.get("/api/notes", function(req, res) {
-    return res.json(data);
-  });
+var data = require("../db/db.json");
+//var data = fs.readFileSync("../db/db.json", "utf-8");
+
+
+// notes
+router.get("/api/notes", function (req, res) {
+  return res.json(data);
+});
+
+// new notes- takes in JSON input
+router.post("/api/notes", function (req, res) {
+  var newNote = req.body;
   
-  // new notes- takes in JSON input
-  app.post("/api/notes", function(req, res) {
-    var newNote = req.body;
-    console.log(newNote);
-    characters.push(newNote);
-    res.json(newNote);
-  });
+
+  var lastId = data[data.length - 1].id;
+  newNote.id = lastId + 1;
+
+  console.log(newNote);
+
+  data.push(newNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(data, 0, 1, 2));
+
+  res.json(newNote);
+});
+
+router.delete("/api/notes/:id", function (req, res) {
+  var deleteNote = data.find(c=> c.id === parseInt(req.params.id));
+  var index = data.indexOf(deleteNote);
+  data.splice(index);
   
-  app.delete("/api/notes/:id", function(req, res) {
-    const index = notes.indexOf(note);
-    notes.splice(index, 1);
-  });
-  
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
-
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // routes
-const htmlRoutes = require("./routes/html")
+const htmlRoutes = require("./routes/html");
+app.use(htmlRoutes);
 
+const apiRoutes = require("./routes/api");
+app.use(apiRoutes);
 
 // listening
-app.listen(PORT, function() {
-    console.log("You are listening on PORT " + PORT);
-  });
-  
+app.listen(PORT, function () {
+  console.log("You are listening on PORT " + PORT);
+});
