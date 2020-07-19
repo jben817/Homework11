51039b2058cce0ef1698c99f6fc372506aa8d44e
j.bennett <61255945+jben817@users.noreply.github.com>
2020-07-19 18:10:23 -0400

    console.log

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const fs = require("fs");
 
-var data = require("./db/db.json");
+var data = require("../db/db.json");
 console.log(data)
 
 // notes
 router.get("/api/notes", function (req, res) {
+  fs.readFile("./db/db.json", function(file){
+    console.log(file)
+  })
   return res.json(data);
 });
 
