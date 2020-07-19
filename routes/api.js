9018011e5b93cc253c4a5a8b568e964dbdfb6583
j.bennett <61255945+jben817@users.noreply.github.com>
2020-07-19 18:22:05 -0400

    console.log

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,7 @@ console.log(data)
 
 // notes
 router.get("/api/notes", function (req, res) {
-  fs.readFile("./db/db.json", function(file){
-    console.log(file)
-  })
+
   return res.json(data);
 });
 
@@ -29,11 +27,20 @@ router.post("/api/notes", function (req, res) {
   res.json(newNote);
 });
 
-// router.delete("/api/notes/:id", function (req, res) {
-//   var deleteNote = data.find(c=> c.id === parseInt(req.params.id));
-//   var index = data.indexOf(deleteNote);
-//   data.splice(index);
+router.delete("/api/notes/:id", function (req, res) {
+  console.log("id:",req.params.id)
+  //var deleteNote = data.find(c=> c.id === parseInt(req.params.id));
+ // var index = data.indexOf(deleteNote);
+//  data.splice(index);
+
+  var newNotes = data.filter(elem=> elem.id !== parseInt(req.params.id))
+  console.log(newNotes)
+  data = newNotes
+  // you need to rewrite the file
+  fs.writeFileSync("./db/db.json", JSON.stringify(data, 0, 1, 2));
+
+  res.json(data);
   
-// });
+});
 
 module.exports = router;
